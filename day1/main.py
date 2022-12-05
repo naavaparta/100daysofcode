ce6cb4f6957f4afae728135791be928c7ea0f585
Naava Sahi <naavasahi@kamk.fi>
2022-12-05 18:49:08 +0200

    first day done

---
+++ day1/main.py
@@ -0,0 +1,5 @@
+print("Hello there! So you want to name your band? I can help!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+
+print("Your band name could be " + city + " " + pet + "!")
